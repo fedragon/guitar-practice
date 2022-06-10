7d083177a76fb2aa552622bbefbc37b9a242e1a0
Federico Ragona <federico.ragona@gmail.com>
2022-06-11 01:07:48 +0200

    Configure chord

--- components/guitar.tsx
+++ components/guitar.tsx
@@ -1,36 +1,66 @@
-export default function Guitar() {
+export default function Guitar({
+  chord
+}: {
+  chord: [number, number, boolean][]
+}) {
   let base = 75
-  let nutOffset = base
+  let nutoff = base + 25
   let width = base * 8
   let height = base * 5
 
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 840 700" >
-      { /* open strings */}
-      <line x1={0} y1={height - base / 6} x2={base / 3 * 2} y2={height - base / 6 * 5} stroke={"black"} strokeWidth={3} />
-      <line x1={base / 3 * 2} y1={height - base / 6} x2={0} y2={height - base / 6 * 5} stroke={"black"} strokeWidth={3} />
-
       {/* nut */}
-      <line x1={nutOffset} y1={0} x2={nutOffset} y2={height} stroke={"black"} strokeWidth={15} />
+      <line x1={nutoff} y1={0} x2={nutoff} y2={height} stroke={"black"} strokeWidth={10} />
 
       {/* frets */}
-      <line x1={nutOffset + width / 4} y1={0} x2={nutOffset + width / 4} y2={height} stroke={"black"} strokeWidth={2} />
-      <line x1={nutOffset + width / 2} y1={0} x2={nutOffset + width / 2} y2={height} stroke={"black"} strokeWidth={2} />
-      <line x1={nutOffset + width / 4 * 3} y1={0} x2={nutOffset + width / 4 * 3} y2={height} stroke={"black"} strokeWidth={2} />
-      <line x1={nutOffset + width} y1={0} x2={nutOffset + width} y2={height} stroke={"black"} strokeWidth={2} />
+      <line x1={nutoff + width / 4} y1={0} x2={nutoff + width / 4} y2={height} stroke={"black"} strokeWidth={2} />
+      <line x1={nutoff + width / 2} y1={0} x2={nutoff + width / 2} y2={height} stroke={"black"} strokeWidth={2} />
+      <line x1={nutoff + width / 4 * 3} y1={0} x2={nutoff + width / 4 * 3} y2={height} stroke={"black"} strokeWidth={2} />
+      <line x1={nutoff + width} y1={0} x2={nutoff + width} y2={height} stroke={"black"} strokeWidth={2} />
 
       {/* strings */}
-      <line x1={nutOffset} y1={0} x2={nutOffset + width} y2={0} stroke={"black"} strokeWidth={2} />
-      <line x1={nutOffset} y1={base} x2={nutOffset + width} y2={base} stroke={"black"} strokeWidth={2} />
-      <line x1={nutOffset} y1={base * 2} x2={nutOffset + width} y2={base * 2} stroke={"black"} strokeWidth={2} />
-      <line x1={nutOffset} y1={base * 3} x2={nutOffset + width} y2={base * 3} stroke={"black"} strokeWidth={2} />
-      <line x1={nutOffset} y1={base * 4} x2={nutOffset + width} y2={base * 4} stroke={"black"} strokeWidth={2} />
-      <line x1={nutOffset} y1={height} x2={nutOffset + width} y2={height} stroke={"black"} strokeWidth={2} />
+      <line x1={nutoff} y1={0} x2={nutoff + width} y2={0} stroke={"black"} strokeWidth={2} />
+      <line x1={nutoff} y1={base} x2={nutoff + width} y2={base} stroke={"black"} strokeWidth={2} />
+      <line x1={nutoff} y1={base * 2} x2={nutoff + width} y2={base * 2} stroke={"black"} strokeWidth={2} />
+      <line x1={nutoff} y1={base * 3} x2={nutoff + width} y2={base * 3} stroke={"black"} strokeWidth={2} />
+      <line x1={nutoff} y1={base * 4} x2={nutoff + width} y2={base * 4} stroke={"black"} strokeWidth={2} />
+      <line x1={nutoff} y1={height} x2={nutoff + width} y2={height} stroke={"black"} strokeWidth={2} />
 
-      {/* chord */}
-      <circle cx={nutOffset + base} cy={base} r={20} stroke={"black"} strokeWidth={2} fill={"red"} />
-      <circle cx={nutOffset + base * 3} cy={base * 3} r={20} stroke={"black"} strokeWidth={2} fill={"red"} />
-      <circle cx={nutOffset + base * 5} cy={base * 4} r={20} stroke={"black"} strokeWidth={2} fill={"red"} />
+      {chord.map(function (row: [number, number, boolean]) {
+        const [fret, str, strum] = row
+
+        let gstring = str - 1
+
+        if (strum) {
+          let cx = nutoff / 3
+          let cy = base * gstring
+          let radius = base / 3
+          let fill = "red"
+
+          if (fret == 0) {
+            fill = "white"
+            radius = base / 3
+          } else if (fret == 1) {
+            cx = nutoff + base
+          } else if (fret > 1) {
+            cx = nutoff + base * (fret + 1) + base * (fret - 2)
+          }
+
+          return (<circle cx={cx} cy={cy} r={radius} stroke={"black"} strokeWidth={2} fill={fill} />)
+        } else {
+          let x = base / 1.5
+          let y1 = base * (gstring - 1) * 1.05
+          let y2 = base * gstring * 0.95
+
+          return (
+            [
+              <line x1={10} y1={y1} x2={x+10} y2={y2} stroke={"black"} strokeWidth={3} />,
+              <line x1={x+10} y1={y1} x2={10} y2={y2} stroke={"black"} strokeWidth={3} />
+            ]
+          )
+        }
+      })}
     </svg>
   )
 }
