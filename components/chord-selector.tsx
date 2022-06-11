644939802d48946159d87b78cebe3dacd80f2f05
Federico Ragona <federico.ragona@gmail.com>
2022-06-11 22:36:28 +0200

    Extract chord selection into component

---
+++ components/chord-selector.tsx
@@ -0,0 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import { useState } from 'react'
+import { Button, ButtonGroup, Stack } from 'react-bootstrap'
+import { AllChords } from '../components/chords'
+import Fretboard from '../components/fretboard'
+
+export function ChordSelector({ onClick, rightSide }) {
+  const [chord, setChord] = useState(AllChords[0])
+
+  return (
+    <Stack>
+      <Stack direction={"horizontal"} gap={2}>
+        <ButtonGroup>
+          {AllChords.map(function (chord) {
+            return (
+              <Button
+                key={chord.name}
+                variant={"outline-primary"}
+                size={"sm"}
+                onClick={() => { setChord(chord); onClick() }}>
+                {chord.name}
+              </Button>
+            )
+          })}
+        </ButtonGroup>
+      </Stack>
+      <Stack direction={"horizontal"} gap={3}>
+        <Fretboard key={"fretboard"} config={{ base: 45 }} chord={chord} />
+        {rightSide}
+      </Stack>
+    </Stack>
+  )
+}
--- pages/chord-perfect.tsx
+++ pages/chord-perfect.tsx
@@ -2,12 +2,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPause, faPlay, faStop } from '@fortawesome/free-solid-svg-icons'
 import { useEffect, useState } from 'react'
-import { Button, ButtonGroup, Container, Stack } from 'react-bootstrap'
-import { AllChords } from '../components/chords'
-import Fretboard from '../components/fretboard'
+import { Button, Stack } from 'react-bootstrap'
+import { ChordSelector } from '../components/chord-selector'
 
 export default function ChordPerfect() {
-  const [chord, setChord] = useState(AllChords[0])
   const [isTimerActive, setTimer] = useState(false)
   const [timeLeft, setTimeLeft] = useState(60)
 
@@ -26,41 +24,28 @@ export default function ChordPerfect() {
     setTimeLeft(60)
   }
 
-  return (
+  let rightSide = (
     <Stack>
-      <Stack direction={"horizontal"} gap={2}>
-        <ButtonGroup>
-          {AllChords.map(function (chord) {
-            return (
-              <Button
-                key={chord.name}
-                variant={"outline-primary"}
-                size={"sm"}
-                onClick={() => { setChord(chord); resetTimer() }}>
-                {chord.name}
-              </Button>
-            )
-          })}
-        </ButtonGroup>
-      </Stack>
-      <Stack direction={"horizontal"} gap={3}>
-        <Fretboard key={"fretboard"} config={{ base: 45 }} chord={chord} />
-        <Stack>
-          <div key={"timeLeftLabel"}>Time left</div>
-          <div key={"timeLeft"} style={{ fontSize: 48 }}>{timeLeft}s</div>
-          <Stack direction={"horizontal"} gap={1}>
-            <Button key={"play"} onClick={() => { setTimer(true) }} disabled={isTimerActive}>
-              <FontAwesomeIcon icon={faPlay} />
-            </Button>
-            <Button key={"pause"} onClick={() => { setTimer(false) }} disabled={!isTimerActive}>
-              <FontAwesomeIcon icon={faPause} />
-            </Button>
-            <Button key={"stop"} onClick={() => { resetTimer() }}>
-              <FontAwesomeIcon icon={faStop} />
-            </Button>
-          </Stack>
-        </Stack>
+      <div key={"timeLeftLabel"}>Time left</div>
+      <div key={"timeLeft"} style={{ fontSize: 48 }}>{timeLeft}s</div>
+      <Stack direction={"horizontal"} gap={1}>
+        <Button key={"play"} onClick={() => { setTimer(true) }} disabled={isTimerActive}>
+          <FontAwesomeIcon icon={faPlay} />
+        </Button>
+        <Button key={"pause"} onClick={() => { setTimer(false) }} disabled={!isTimerActive}>
+          <FontAwesomeIcon icon={faPause} />
+        </Button>
+        <Button key={"stop"} onClick={() => { resetTimer() }}>
+          <FontAwesomeIcon icon={faStop} />
+        </Button>
       </Stack>
     </Stack>
+  )
+
+  return (
+    <ChordSelector
+      onClick={() => resetTimer()}
+      rightSide={rightSide}
+    />
   )
 }
