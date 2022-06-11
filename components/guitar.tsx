81bfe82d1e0465d8cc67ee3bf34c3cad511102e5
Federico Ragona <federico.ragona@gmail.com>
2022-06-11 07:26:21 +0200

    Fix visualization of open/not played strings

--- components/guitar.tsx
+++ components/guitar.tsx
@@ -4,28 +4,29 @@ export default function Guitar({
   chord: [number, number, boolean][]
 }) {
   let base = 75
-  let nutoff = base + 25
+  let xoff = base + 25
+  let yoff = 50
   let width = base * 8
   let height = base * 5
 
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 840 700" >
       {/* nut */}
-      <line x1={nutoff} y1={0} x2={nutoff} y2={height} stroke={"black"} strokeWidth={10} />
+      <line x1={xoff} y1={yoff} x2={xoff} y2={yoff + height} stroke={"black"} strokeWidth={10} />
 
       {/* frets */}
-      <line x1={nutoff + width / 4} y1={0} x2={nutoff + width / 4} y2={height} stroke={"black"} strokeWidth={2} />
-      <line x1={nutoff + width / 2} y1={0} x2={nutoff + width / 2} y2={height} stroke={"black"} strokeWidth={2} />
-      <line x1={nutoff + width / 4 * 3} y1={0} x2={nutoff + width / 4 * 3} y2={height} stroke={"black"} strokeWidth={2} />
-      <line x1={nutoff + width} y1={0} x2={nutoff + width} y2={height} stroke={"black"} strokeWidth={2} />
+      <line x1={xoff + width / 4} y1={yoff} x2={xoff + width / 4} y2={yoff + height} stroke={"black"} strokeWidth={2} />
+      <line x1={xoff + width / 2} y1={yoff} x2={xoff + width / 2} y2={yoff + height} stroke={"black"} strokeWidth={2} />
+      <line x1={xoff + width / 4 * 3} y1={yoff} x2={xoff + width / 4 * 3} y2={yoff + height} stroke={"black"} strokeWidth={2} />
+      <line x1={xoff + width} y1={yoff} x2={xoff + width} y2={yoff + height} stroke={"black"} strokeWidth={2} />
 
       {/* strings */}
-      <line x1={nutoff} y1={0} x2={nutoff + width} y2={0} stroke={"black"} strokeWidth={2} />
-      <line x1={nutoff} y1={base} x2={nutoff + width} y2={base} stroke={"black"} strokeWidth={2} />
-      <line x1={nutoff} y1={base * 2} x2={nutoff + width} y2={base * 2} stroke={"black"} strokeWidth={2} />
-      <line x1={nutoff} y1={base * 3} x2={nutoff + width} y2={base * 3} stroke={"black"} strokeWidth={2} />
-      <line x1={nutoff} y1={base * 4} x2={nutoff + width} y2={base * 4} stroke={"black"} strokeWidth={2} />
-      <line x1={nutoff} y1={height} x2={nutoff + width} y2={height} stroke={"black"} strokeWidth={2} />
+      <line x1={xoff} y1={yoff} x2={xoff + width} y2={yoff} stroke={"black"} strokeWidth={2} />
+      <line x1={xoff} y1={yoff + base} x2={xoff + width} y2={yoff + base} stroke={"black"} strokeWidth={2} />
+      <line x1={xoff} y1={yoff + base * 2} x2={xoff + width} y2={yoff + base * 2} stroke={"black"} strokeWidth={2} />
+      <line x1={xoff} y1={yoff + base * 3} x2={xoff + width} y2={yoff + base * 3} stroke={"black"} strokeWidth={2} />
+      <line x1={xoff} y1={yoff + base * 4} x2={xoff + width} y2={yoff + base * 4} stroke={"black"} strokeWidth={2} />
+      <line x1={xoff} y1={yoff + height} x2={xoff + width} y2={yoff + height} stroke={"black"} strokeWidth={2} />
 
       {chord.map(function (row: [number, number, boolean]) {
         const [fret, str, strum] = row
@@ -33,8 +34,8 @@ export default function Guitar({
         let gstring = str - 1
 
         if (strum) {
-          let cx = nutoff / 3
-          let cy = base * gstring
+          let cx = xoff / 3
+          let cy = yoff + base * gstring
           let radius = base / 3
           let fill = "red"
 
@@ -42,21 +43,21 @@ export default function Guitar({
             fill = "white"
             radius = base / 3
           } else if (fret == 1) {
-            cx = nutoff + base
+            cx = xoff + base
           } else if (fret > 1) {
-            cx = nutoff + base * (fret + 1) + base * (fret - 2)
+            cx = xoff + base * (fret + 1) + base * (fret - 2)
           }
 
           return (<circle cx={cx} cy={cy} r={radius} stroke={"black"} strokeWidth={2} fill={fill} />)
         } else {
           let x = base / 1.5
-          let y1 = base * (gstring - 1) * 1.05
-          let y2 = base * gstring * 0.95
+          let y1 = yoff + base * gstring * 1.05
+          let y2 = yoff + base * gstring * 0.95
 
           return (
             [
-              <line x1={10} y1={y1} x2={x+10} y2={y2} stroke={"black"} strokeWidth={3} />,
-              <line x1={x+10} y1={y1} x2={10} y2={y2} stroke={"black"} strokeWidth={3} />
+              <line x1={10} y1={y1} x2={x + 10} y2={y2} stroke={"black"} strokeWidth={3} />,
+              <line x1={x + 10} y1={y1} x2={10} y2={y2} stroke={"black"} strokeWidth={3} />
             ]
           )
         }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,14 @@ import Layout from '../components/layout'
 export default function Home() {
   return (
     <Layout home>
-      <Guitar chord={[[0, 1, true], [1, 2, true], [2, 4, true], [3, 5, true], [0, 3, true], [0, 6, false]]} />
+      <Guitar chord={[
+        [0, 1, true],
+        [1, 2, true],
+        [2, 4, true],
+        [3, 5, true],
+        [0, 3, true],
+        [0, 6, false]
+      ]} />
     </Layout>
   )
 }
